38fff53d56227e2c184233b30a0b824d7e197f72
Doniben <doniben@esperanto.co>
2019-09-10 20:15:46 -0400

    [FEATURE] printing numbers from 1 to 100 using fizzbux=zz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,5 +4,5 @@ def fizzbuzz():
         if i % 3 != 0 and i % 5 != 0:
             print("{:d}".format(i), end=' ')
         else:
-            print("{}".format("Fizz" if ((i % 3) == 0) else ""), end=' ')
+            print("{}".format("Fizz" if ((i % 3) == 0) else ""), end='')
             print("{}".format("Buzz" if ((i % 5) == 0) else ""), end=' ')
