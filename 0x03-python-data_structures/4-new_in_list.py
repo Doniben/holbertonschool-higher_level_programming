60a6bb918f1f093065056e687b28470437a0e2ad
Doniben <doniben@esperanto.co>
2019-09-16 23:41:46 -0400

    [FEATURE] unction that replaces an element in a list at a specific position

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list is not None:
+        new_list = my_list.copy()
+        if len(new_list) > idx and idx >= 0:
+            new_list[idx] = element
+            return new_list
