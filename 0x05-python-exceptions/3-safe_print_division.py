a15df9038178fa049bf46ecc7d75b720ba782a8c
Doniben <Doniben@esperanto.co>
2019-09-30 21:58:07 -0400

    [FEATURE] divides 2 integers and prints the result.

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    div = None
+    try:
+        div = a / b
+        return div
+
+    except ZeroDivisionError:
+        return(None)
+    finally:
+        print("Inside result: {0}".format(div))
