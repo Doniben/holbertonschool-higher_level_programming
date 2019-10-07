2816e289a453e70161027bde1e302fa3aa6be523
Doniben <Doniben@esperanto.co>
2019-10-07 17:07:51 -0400

    [FEATURE] class Rectangle that defines a rectangle by: (based on 0-rectangle.py)+trying to change side of the rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,7 +3,20 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
- 
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
     @property
     def width(self):
         return self.__width
