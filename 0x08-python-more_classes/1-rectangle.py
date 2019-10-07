a8e91813114184070625ca5c736fc69a7788a73a
Doniben <Doniben@esperanto.co>
2019-10-07 17:12:43 -0400

    [FEATURE] class Rectangle that defines a rectangle by: (based on 0-rectangle.py)+Change in the printed side addding private values in the init

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def height(self):
@@ -27,18 +27,5 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:        
-            self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
         else:
-            self.__height = value
+            self.__width = value
