f20bc06670d0fbdda4e779722201078a611d224a
Doniben <Doniben@esperanto.co>
2019-10-07 19:50:07 -0400

    [FEATURE] class Rectangle that defines a rectangle by: (based on 1-rectangle.py) + adding 0 conditions + else + turning order

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,19 +5,6 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self):
-        return self.__height
-
-    @width.setter
-    def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
-
-    @property
     def height(self):
         return self.__height
 
@@ -30,14 +17,24 @@ class Rectangle:
         else:
             self.__height = value
 
-    def area(self):
-        if self.__width == 0 or self.__height == 0:
-            return 0
+    @property
+    def width(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
         else:
-            return self.__width * self.__height
+            self.__width = value
+
+    def area(self):
+        return self.__width * self.__height
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        else:        
+        else:
             return 2 * (self.__width + self.__height)
