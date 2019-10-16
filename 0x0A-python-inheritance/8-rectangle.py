09a55785875878dc2aabc5b78daec7e5542c95ca
Doniben <doniben@esperanto.co>
2019-10-16 01:39:24 +0000

    [FEATURE] class Rectangle that inherits from BaseGeometry (7-base_geometry.py).

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,7 +3,7 @@
 
 class BaseGeometry:
     def area(self):
-        raise Exception("area() is not implementes")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if type(value) != int:
