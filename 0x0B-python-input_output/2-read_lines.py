b63bc5afc43a0e8ac3d68f1ae729c7950d9faca0
Doniben <doniben@esperanto.co>
2019-10-16 23:19:45 +0000

    [FEATURE] function that reads n lines of a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,7 +3,7 @@
 
 def read_lines(filename="", nb_lines=0):
     counter = 0
-    with open(filename="", encoding="utf-8") as F:
+    with open(filename, encoding="utf-8") as F:
         for line in F:
             counter += 1
             if nb_lines <= 0 or nb_lines >= counter:
