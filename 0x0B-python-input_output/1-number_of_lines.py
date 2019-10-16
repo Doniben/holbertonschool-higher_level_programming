686f91c316dba13b9b9c9855707bacdbda78679d
Doniben <doniben@esperanto.co>
2019-10-16 21:58:11 +0000

    [FEATURE] function that returns the number of lines of a text file + change inside loop

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -5,5 +5,5 @@ def number_of_lines(filename=""):
     with open(filename, encoding="UTF-8") as F:
         size = 0
         for line in F:
-            size + size + 1
+            size = size + 1
         return size
