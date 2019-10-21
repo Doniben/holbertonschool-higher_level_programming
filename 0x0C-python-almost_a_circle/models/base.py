889ac362422ea36739533117a0ed3a0632243dcd
Doniben <doniben@esperanto.co>
2019-10-21 18:32:34 +0000

    [FEATURE] Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+class Base:
+    """ FIRST CLASS """
+    __nb_objects = 0
+    """ private class attribute """
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        """ assign the public instance attribute id with it value """
+    else:
+        __nb_objects += 1
+    self.id = Base.__nb_objects
+    """ assign the new value to the public instance attribute id """
