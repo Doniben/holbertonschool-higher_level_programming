634b214baf3f8aa1077a25cdb4fe8d316268144f
Doniben <doniben@esperanto.co>
2019-10-22 14:49:49 +0000

    [FEATURE] adde for tasks 11 and 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,3 +12,16 @@ class Square(Rectangle):
     def __str__(self):
          return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
                  self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+         return self.width
+
+    @size.setter
+    def size(self, value):
+         if not isinstance(value, int):
+             raise TypeError("width must be an integer")
+         elif value <= 0:
+             raise ValueError("width must be > 0")
+         self.width = value
+         self.height = value
