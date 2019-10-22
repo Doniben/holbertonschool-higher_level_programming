558f79deb495db05e1e63b34803903e0200f4893
Doniben <doniben@esperanto.co>
2019-10-22 11:21:44 +0000

    [FEATURE] handled Errors+ adding elif + changing first initialized

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,10 +5,10 @@ from models.base import Base
 class Rectangle(Base):
     """ DEFINES CLASS """
     def __init__(self, width, height, x=0, y=0, id=None):
-        __width = width
-        __height = height
-        __x = x
-        __y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
