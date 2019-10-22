27fc8a1a682e57193bbe0976e5080bb7332e32d3
Doniben <doniben@esperanto.co>
2019-10-22 02:23:44 +0000

    [FEATURE] Base class that manage id attribute in all future classes + indents

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,7 +10,7 @@ class Base:
         if id is not None:
             self.id = id
         """ assign the public instance attribute id with it value """
-    else:
-        __nb_objects += 1
-    self.id = Base.__nb_objects
-    """ assign the new value to the public instance attribute id """
+        else:
+            __nb_objects += 1
+        self.id = Base.__nb_objects
+        """ assign the new value to the public instance attribute id """
