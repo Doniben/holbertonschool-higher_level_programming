d3b6bff6d77a8dbff696b42b4c97945a4dec1b75
Doniben <doniben@esperanto.co>
2019-10-22 05:06:50 +0000

    [FEATURE] Class rectangle that inherits from Base with 4 private instance atributes

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ DEFINES CLASS """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        __width = width
+        __height = height
+        __x = x
+        __y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        self.__with = value
+
+    @property
+    def height(self):
+        return self.__width
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, value):
+        self.__y = value
