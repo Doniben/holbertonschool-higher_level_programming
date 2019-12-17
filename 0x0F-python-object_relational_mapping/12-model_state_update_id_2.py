fe0ea26042b05d80ad4066a422b0728bc2e87dec
Doniben <doniben@esperanto.co>
2019-12-17 23:40:54 +0000

    [FEATURE] script that changes the name of a State object from the database

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -12,13 +12,14 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    # generates new Session objects when called 
     Session = sessionmaker(bind=engine)
     mySession = Session()
 
     query = mySession.query(State)
 
-    for elem in query:
-        if elem.id == 2:
-            elem.name = "New Mexico"
+    for element in query:
+        if element.id == 2:
+            element.name = "New Mexico"
 
     mySession.commit()
