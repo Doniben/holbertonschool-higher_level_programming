8beb37518c0748fc239feedb2ce6f0ab43f99398
Doniben <doniben@esperanto.co>
2019-12-17 19:21:20 +0000

    [FETUARE] script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,10 +12,12 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    # generates new Session objects when called
     Session = sessionmaker(bind=engine)
     mySession = Session()
     myquer = mySession.query(State).order_by(State.id)
 
     for elem in myquer:
+        #Print the cities that match with a
         if 'a' in elem.name:
             print("{}: {}".format(elem.id, elem.name))
