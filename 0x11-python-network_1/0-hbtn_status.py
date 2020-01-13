43d5db0ba805a7a32f3622bd28a23bd99fffdf71
Doniben <doniben@esperanto.co>
2020-01-13 22:26:54 +0000

    [FEATURE] Python script that fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status """
+
+import urllib.request
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    status = response.read()
+    print("Body response:$")
+    print("\t- type: {}$".format(type(status)))
+    print("\t- content: {}$".format(status))
+    print("\t- utf8 content: {}$".format(status.decode('utf8')))
