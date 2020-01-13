d3324630c57b2e36340a0da6583ca6e3136b6e9d
Doniben <doniben@esperanto.co>
2020-01-13 22:29:22 +0000

    [FEATURE] Python script that fetches https://intranet.hbtn.io/status + change in dollar sign

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     status = response.read()
-    print("Body response:$")
-    print("\t- type: {}$".format(type(status)))
-    print("\t- content: {}$".format(status))
-    print("\t- utf8 content: {}$".format(status.decode('utf8')))
+    print("Body response:")
+    print("\t- type: {}".format(type(status)))
+    print("\t- content: {}".format(status))
+    print("\t- utf8 content: {}".format(status.decode('utf8')))
