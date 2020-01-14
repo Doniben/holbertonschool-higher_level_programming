91de4ec9651dd82589366eefa3ec97091b74d907
Doniben <doniben@esperanto.co>
2020-01-14 05:42:35 +0000

    [FEATURE] Python script that takes in a URL, sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-""" perr """
+""" Python script that takes in a URL, sends a request to the URL and
+displays the body of the response. """
 
+from requests.exceptions import HTTPError
 import requests
 import sys
 
 if __name__ == "__main__":
-    pass
+    try:
+        arg = requests.get(sys.argv[1])
+        print(arg.text)
+    except HTTPError as err
+        print("Error code: {}".format(err))
