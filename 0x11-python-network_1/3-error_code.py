7222732eb9c8a8eefeaad144cc02ac68d6d9071a
Doniben <doniben@esperanto.co>
2020-01-14 02:45:41 +0000

    [FEATURE] Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8). """
+
+if __name__ == "__main__":
+
+    try:
+        arg = urllib.request.urlopen(sys.argv[1])
+        with arg as response:
+            print(response.read().decode())
+
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
