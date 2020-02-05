c89b1d79c458dc213517a8cc495da676ffdbd66b
Doniben <doniben@esperanto.co>
2020-02-05 03:42:12 +0000

    [FEATURE] array multilanguage

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let count = 0;
+const aum = array.length;
+
+while (count < aum) {
+  console.log(array[count]);
+  count += 1;
+}
