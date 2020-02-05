23fbb262af42b4cd5d7428f2dc91ac636efd2e46
Doniben <doniben@esperanto.co>
2020-02-05 02:42:43 +0000

    [FEATURE] arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-if (!process.argv[2])
-    console.log('No argument')
-else
-{
-    if (process.argv[2] == 1)
-	console.log('Argument found')
-    else
-	console.log('Arguments found')
+if (!process.argv[2]) {
+  console.log('No argument');
+} else {
+  if (process.argv.length > 3) {
+    console.log('Arguments found');
+  } else {
+    console.log('Argument found');
+  }
 }
