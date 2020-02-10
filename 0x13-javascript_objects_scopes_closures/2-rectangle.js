a167e7433f145ba67d7929dd86fc3a8260510c1d
Doniben <doniben@esperanto.co>
2020-02-10 23:33:01 +0000

    [FEATURE] class Rectangle that defines a rectangle with values, integrer, and 0 validation

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,12 @@
 
 class Rectangle {
   constructor (w, h) {
-      if (Number.isInteger(w) && Number.isInteger(h)) {
-	    if (w > 0 && h > 0) {
-		this.width = w;
-		this.height = h;
-	    }
-      }
+    if (Number.isInteger(w) && Number.isInteger(h)) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+	}
+    }
   }
 }
 module.exports = Rectangle;
