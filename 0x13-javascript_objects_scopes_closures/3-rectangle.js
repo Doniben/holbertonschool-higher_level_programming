c4bd806bf879f4dd3046022610087b71255cbc7d
Doniben <doniben@esperanto.co>
2020-02-11 03:54:48 +0000

    [FEATURE]  class Rectangle that defines a rectangle with print function

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+module.exports =
+
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && Number.isInteger(h)) {
+	    if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+	    }
+    }
+  }
+
+  print () {
+    while (this.height) {
+      console.log('X'.repeat(this.width));
+      this.height--;
+    }
+  }
+};
